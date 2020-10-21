65b02f255270faa2bd6e568b57bcf08b1e585260
Emily Trinh <emilooo@uw.edu>
2020-10-20 20:34:02 -0700

    graph

--- assignment.R
+++ assignment.R
@@ -2,22 +2,10 @@ rm(list = ls())
 library(dplyr)
 library(ggplot2)
 
-life_df <- read.csv("data/life_expectancy.csv", stringsAsFactors = FALSE)
-child_df <- read.csv("data/child_mortality.csv", stringsAsFactors = FALSE)
-
-col2rowlife_df <- life_df <- as.data.frame(t(life_df))
-col2rowchild_df <- child_df <- as.data.frame(t(child_df))
+US_life_df <- read.csv("data/us_life_expectancy.csv", stringsAsFactors = FALSE)
+US_child_df <- read.csv("data/us_mortality.csv", stringsAsFactors = FALSE)
 
 
-US_life_expectancy <- col2rowlife_df %>% 
-  select(V207)
 
-names(US_life_expectancy) <- "United States"
-
-US_child_mortality <- col2rowchild_df %>% 
-  select(V207)
-
-names(US_child_mortality) <- "United States"
-  
-ggplot(data = US_life_expectancy, aes(x=, y= age)) + geom_point()
+ggplot(data = life_df, aes(x= year, y= life_expectancy)) + geom_point()
 
