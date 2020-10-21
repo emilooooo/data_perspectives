6ba99b1d808ff46e79d6baf83ab88280c02c9132
Emily Trinh <emilooo@uw.edu>
2020-10-20 19:20:33 -0700

    filtered to US life expectancy/child mortality git push

--- assignment.R
+++ assignment.R
@@ -1,5 +1,19 @@
 rm(list = ls())
 library(dplyr)
+library(ggplot2)
 
 life_df <- read.csv("data/life_expectancy.csv", stringsAsFactors = FALSE)
 child_df <- read.csv("data/child_mortality.csv", stringsAsFactors = FALSE)
+
+col2rowlife_df <- life_df <- as.data.frame(t(life_df))
+col2rowchild_df <- child_df <- as.data.frame(t(child_df))
+
+
+US_life_expectancy <- col2rowlife_df %>% 
+  select(V207)
+
+US_child_mortality <- col2rowchild_df %>% 
+  select(V207)
+  
+
+
