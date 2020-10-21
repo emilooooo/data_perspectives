4141ab3916a79e593c9fa8c3adf41740bf11e762
Emily Trinh <emilooo@uw.edu>
2020-10-20 20:38:51 -0700

    updated

--- assignment.R
+++ assignment.R
@@ -5,7 +5,19 @@ library(ggplot2)
 US_life_df <- read.csv("data/us_life_expectancy.csv", stringsAsFactors = FALSE)
 US_child_df <- read.csv("data/us_mortality.csv", stringsAsFactors = FALSE)
 
+CA_life_df <- read.csv("data/canada_life_expectancy.csv", stringsAsFactors = FALSE)
+CA_child_df <- read.csv("data/canada_mortality.csv", stringsAsFactors = FALSE)
 
 
-ggplot(data = life_df, aes(x= year, y= life_expectancy)) + geom_point()
+#US life
+ggplot(data = US_life_df, aes(x= year, y= life_expectancy)) + geom_point()
+
+#US child
+ggplot(data = US_child_df, aes(x= year, y= mortality)) + geom_point()
+
+#CA life
+ggplot(data = CA_life_df, aes(x= year, y= life_expectancy)) + geom_point()
+
+#CA child
+ggplot(data = CA_child_df, aes(x= year, y= mortality)) + geom_point()
 
